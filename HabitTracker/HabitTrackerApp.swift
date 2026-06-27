adb5d028dbd6b875b27cc1590b9d0d590a54a288
midkids <83714759+midkids@users.noreply.github.com>
2026-06-27 11:18:44 -0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Myron Snelson on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
