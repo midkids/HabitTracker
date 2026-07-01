ee65bff80a6b13fd29aaaf04f4c6dc9786db53c1
midkids <83714759+midkids@users.noreply.github.com>
2026-07-01 15:40:53 -0400

    Added a detail view to show the habit description

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -7,121 +7,13 @@
 
 import SwiftUI
 
+// This project is from the challenges of
+// Day 47. It was based on the iExpense app,
+// but with elements of the Navigation added
+// to it. For detailed explanations of the
+// HabitTracker app logic, you may need to
+// reference these two apps.
 
-
-/*
-// Storing user settings with UserDefaults
-//
-// UserDefaults can store integers, strings, Booleans, and more
-// The simplest way to read and write a small of data
-// is through user defaults
-// It is a great way to keep user preferences
-// Storing too many user defaults will slow the
-// loading of your app
-// Should store no more than 512 KB
-// Their values are automatically loaded when our app starts
-//   so try not to save too much
-// Should store no more than 512 KB
-// IMPORTANT: UserDefaults uses strings for its key names.
-
-struct ContentView: View {
-    
-//    @State private var tapCount = 0
-    
-    // If the key cannot be found (as in the first time
-    // the app runs), the value of Tap defaults to 0
-    // because it is an integer
-    // Booleans default to false
-    // iOS buffers writing default values and
-    // it can take a few seconds to write all the user defaults
-    // However, in theory, nothing will ever be lost
-//    @State private var tapCount =
-//     UserDefaults.standard.integer(forKey: "Tap")
-    
-    // IMPORTANT: App storage is a MUCH better choice
-    // than user defaults (user defaults actually works
-    // by using app storage)
-    // When the value of tapCount changes,
-    //   SwiftUI will reinvoke the body property automatically
-    //   This makes sure the user interface always
-    //   reflects the latest value
-    // The default value is used if there is no value set
-    @AppStorage("tapCount") private var tapCount = 0
-    
-    var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
-            // Standard is the built in instance of User Defaults
-            // that is attached to our apps (can create your own)
-            // Single set method and
-            // It can store integers, strings, booleans
-            // Must give a string name (e.g. "Tap")
-            // to the value we are writing
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
-        }
-    }
-}
- */
-
-/*
-// Archiving Swift objects with Codable
-//
-// For complex data types such as
-//   custom Swift types
-// Here we have simple text, but could also have
-//   other simple types (e.g. integers, booleans, doubles,
-//   arrays, and dictionaries)
-// IMPORTANT: use protocol Codable
-//   which is responsible for archiving and unarchiving data
-//   that means it can convert objects like this one
-//   into plain text and back again
-// NEW TYPE: JSON - JavaScript Object Notation
-// is the most common type used with Codable
-// Our expense items are ready to be stored
-struct User: Codable {
-    let firstName: String
-    let lastName: String
-}
-
-struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-    var body: some View {
-        Button("Save User") {
-            // The value of the data variable
-            // is encoded in type Data
-            // that can hold any type of data
-            // object -> JSON binary data format type Data
-            // To do JSON -> object, we would use JSONDecoder
-            let encoder = JSONEncoder()
-            if let data = try? encoder.encode(user) {
-                
-                UserDefaults.standard.set(data, forKey: "UserData")
-            }
-        }
-      
-    }
-}
-*/
-
-
-// The actual iExpense project
-// - Building a list we can delete from
-// - Working with identifiable items in SwiftUI
-// - Sharing an observed object with a new view
-// - Making changes permanent with UserDefaults
-// - Final polish
-//
-// Making the struct conform to the protcol Identifiable
-// Lets SwiftUI know this data can be uniquely identifed
-// Identifiable requires a property named id that makes
-// the struct unique
-// IMPORTANT: the JSON encoder can only be used
-//   to encode objects that conform to the
-//   Codeable protocol - added Codeable to HabitItem struct
-// If we add Codable conformance to a type,
-//   Swift can generate archiving and unarchiving code for us.
-// This only works if all the properties inside the type also conform to Codable
-// UUID already conforms to Codable
 struct HabitItem: Identifiable, Codable {
     // This will make a unique id for every entry in the array
     // Had to make id a var to get rid of warning
@@ -178,6 +70,20 @@ class Habits {
     }
 }
 
+struct DetailView: View {
+    @Environment(\.dismiss) var dismiss
+    let description: String
+    var body: some View {
+        Text("Description:")
+            .font(.title)
+        Text(description)
+        Spacer()
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
+}
+
 struct ContentView: View {
     // Using @State here is just to keep the object alive
     //   It is the @Observable macro that notices changes
@@ -185,6 +91,7 @@ struct ContentView: View {
     // IMPORTANT: Both the ContentView and the AddView
     //   will share the same list of expense items
     @State private var habits = Habits()
+    @State private var selectedHabit: HabitItem?
     
     var body: some View {
         NavigationStack {
@@ -218,13 +125,18 @@ struct ContentView: View {
                             HStack {
                                 Text("Habit:")
                                     .font(.headline)
-                                Text(item.title)
-                                //                            Text(item.description)
+                                Button(item.title) {
+                                    selectedHabit = item
+                                }
+                                .foregroundStyle(.blue)
+                                .underline()
+                                .buttonStyle(.plain)
+                
+                                
                                 Spacer()
                                 Text("Streak:")
                                     .font(.headline)
                                 Text("\(item.streak)")
-                                Spacer()
                                 Button("+") {
                                     // This line finds the real habit
                                     //  inside the habits.items array.
@@ -251,6 +163,9 @@ struct ContentView: View {
                 // The onDelete modifier exists only on ForEach
                 // allows swipe left to delete an item
                 .onDelete(perform: removeItems)
+            }
+            .sheet(item: $selectedHabit) { habit in
+                DetailView(description: habit.description)
             }
             .navigationTitle("HabitTracker")
             // Add habits by navigating to the AddView.
